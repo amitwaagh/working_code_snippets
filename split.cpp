a97ff6adcc096b2f3c9a16d5ed96e56388203741
Amit Wagh <vishuwagh@gmail.com>
2021-12-21 13:32:05 -0500

    Update split.cpp
    added namespace

--- split.cpp
+++ split.cpp
@@ -1,48 +1,61 @@
 
-template <class InputIt, class ForwardIt, class BinOp>
-auto for_each_token(InputIt first, InputIt last,
-                    ForwardIt s_first, ForwardIt s_last,
-                    BinOp binary_op)
-    noexcept ( true )
-    -> void {
-        
-    while( first != last ) {
-        auto const pos = std::find_first_of(first, last, s_first, s_last);
-        binary_op( first, pos );
-        if( pos == last ) break;
-        first = std::next( pos );
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace {
+    using std::cout;
+    using std::string;
+    using std::vector;
+   
+    auto constexpr newline {'\n'};
+
+    template <class InputIt, class ForwardIt, class BinOp>
+    auto for_each_token(InputIt first, InputIt last,
+                        ForwardIt s_first, ForwardIt s_last,
+                        BinOp binary_op)
+        noexcept ( true )
+        -> void {
+
+        while( first != last ) {
+            auto const pos = std::find_first_of(first, last, s_first, s_last);
+            binary_op( first, pos );
+            if( pos == last ) break;
+            first = std::next( pos );
+        }
+
     }
-    
-}
 
-auto split( string const& str,
-            string const& delims = " ",
-            bool const skip_empty = true )
-    noexcept ( true )
-    -> vector<string> {
+    auto split( string const& str,
+                string const& delims = " ",
+                bool const skip_empty = true )
+        noexcept ( true )
+        -> vector<string> {
 
-    vector<string> output;
-    for_each_token(cbegin(str), cend(str),
-                   cbegin(delims), cend(delims),
-                   [&result, skip_empty](auto const begin, auto const end) {
-                    if( begin != end || !skip_empty) {
-                        output.emplace_back(begin, end);
-                    }
-                  });
-    
-    return output;
-    
-}
+        vector<string> output;
+        for_each_token(cbegin(str), cend(str),
+                       cbegin(delims), cend(delims),
+                       [&result, skip_empty](auto const begin, auto const end) {
+                        if( begin != end || !skip_empty) {
+                            output.emplace_back(begin, end);
+                        }
+                      });
 
-auto print_vec( auto const& seq_container,
-                char const sep = '|' )
-    noexcept ( true )
-    -> void {
-    cout << " vector = { ";
-    for( auto const& elem : seq_container ) {
-        cout << elem << " " << sep << " ";
+        return output;
+
     }
-    cout << "} " << newline;
+
+    auto print_vec( auto const& seq_container,
+                    char const sep = '|' )
+        noexcept ( true )
+        -> void {
+        cout << " vector = { ";
+        for( auto const& elem : seq_container ) {
+            cout << elem << " " << sep << " ";
+        }
+        cout << "} " << newline;
+    }
 }
 
 auto main( ) noexcept -> decltype( EXIT_SUCCESS ) {
